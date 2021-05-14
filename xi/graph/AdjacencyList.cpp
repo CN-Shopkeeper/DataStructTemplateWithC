eab0193a8c5fd64ae8f1d963460f51083b475630
shopkeeper <734212732@qq.com>
2021-05-14 15:19:49 +0800

    曦：增加了关节点内同

--- xi/graph/AdjacencyList.cpp
+++ xi/graph/AdjacencyList.cpp
@@ -32,6 +32,7 @@ typedef struct{
 }ALGraph;
 
 Status createGraph(ALGraph &g);
+Status createDG(ALGraph &g);
 Status createUDG(ALGraph &g);
 int locateVex(ALGraph g,VertexType v);
 
@@ -46,12 +47,27 @@ void printGraph(ALGraph g){
         printf("%d ",g.vertices[i].data);
     }
     printf("\n有%d条弧，分别为:\n",g.arcNum);
-    for (int i=0;i<g.vexNum;i++){
-        ArcNode *p=g.vertices[i].firstArc;
-        while (p->nextarc) {
-            p=p->nextarc;
-            printf("从%d到%d有一条弧\n",g.vertices[i].data,g.vertices[p->adjvex].data);
+    switch (g.kind){
+    case DG:
+        for (int i=0;i<g.vexNum;i++){
+            ArcNode *p=g.vertices[i].firstArc;
+            while (p->nextarc) {
+                p=p->nextarc;
+                printf("从%d到%d有一条弧\n",g.vertices[i].data,g.vertices[p->adjvex].data);
+            }
         }
+        break;
+    case UDG:
+        for (int i=0;i<g.vexNum;i++){
+            ArcNode *p=g.vertices[i].firstArc;
+            while (p->nextarc) {
+                p=p->nextarc;
+                printf("从%d到%d有一条弧\n",g.vertices[i].data,g.vertices[p->adjvex].data);
+            }
+        }
+        break;
+    default:
+        break;
     }
 }
 
@@ -62,8 +78,21 @@ void printGraph(ALGraph g){
  * @return Status 操作结果 
  */
 Status createGraph(ALGraph &g){
-    printf("构建一个有向图\n");
-    return createUDG(g);
+    printf("输入构造的图的类型（无向图：%d；有向图：%d）\n",UDG,DG);
+    scanf("%d",&g.kind);
+    switch (g.kind){
+    case UDG:
+        printf("构建一个无向图\n");
+        return createUDG(g);
+        break;
+    case DG:
+        printf("构建一个无向网（无弧信息）\n");
+        return createDG(g);
+        break;
+    default:
+        printf("类型错误");
+        return ERROR;
+    }
 }
 
 /**
@@ -72,8 +101,7 @@ Status createGraph(ALGraph &g){
  * @param g 图
  * @return Status 操作结果 
  */
-Status createUDG(ALGraph &g){
-    g.kind=UDG;
+Status createDG(ALGraph &g){
     printf("输入节点数量:\n");
     scanf("%d",&g.vexNum);
     printf("输入弧数量:\n");
@@ -103,6 +131,46 @@ Status createUDG(ALGraph &g){
 }
 
 /**
+ * @brief 创建一个有向图
+ * 
+ * @param g 图
+ * @return Status 操作结果 
+ */
+Status createUDG(ALGraph &g){
+    printf("输入节点数量:\n");
+    scanf("%d",&g.vexNum);
+    printf("输入弧数量:\n");
+    scanf("%d",&g.arcNum);
+    printf("输入各个节点名称:\n");
+    for (int i=0;i<g.vexNum;i++){
+        g.vertices[i].firstArc=(ArcNode *)malloc(sizeof(ArcNode));
+        g.vertices[i].firstArc->nextarc=NULL;
+        g.vertices[i].firstArc->info=NULL;
+        scanf("%d",&g.vertices[i].data);
+        g.mapping[g.vertices[i].data]=i;
+    }
+    printf("输入各个弧（源节点 目标节点）:\n");
+    for (int i=0;i<g.arcNum;i++){
+        int s,d;
+        scanf("%d%d",&s,&d);
+        int is=locateVex(g,s);
+        int id=locateVex(g,d);
+        ArcNode *ps=g.vertices[is].firstArc->nextarc;
+        ArcNode *nexts=(ArcNode *)malloc(sizeof(ArcNode));
+        nexts->nextarc=ps;
+        nexts->adjvex=id;
+        g.vertices[is].firstArc->nextarc=nexts;
+        ArcNode *pd=g.vertices[id].firstArc->nextarc;
+        ArcNode *nextd=(ArcNode *)malloc(sizeof(ArcNode));
+        nextd->nextarc=pd;
+        nextd->adjvex=is;
+        g.vertices[id].firstArc->nextarc=nextd;
+        //不输入弧信息
+    }
+    return OK;
+}
+
+/**
  * @brief 根据节点名称获取其在数据结构中的下标
  * 
  * @param g 图
@@ -113,9 +181,10 @@ int locateVex(ALGraph g,VertexType v){
     return g.mapping[v];
 }
 
-int main(){
-    ALGraph g;
-    createGraph(g);
-    printGraph(g);
-    return 0;
-}
+// int main(){
+//     ALGraph g;
+//     if (createGraph(g)){
+//        printGraph(g); 
+//     }
+//     return 0;
+// }
---
+++ xi/graph/ArticulationPoint.cpp
@@ -0,0 +1,78 @@
+#include"AdjacencyList.cpp"
+
+int counts;
+int visited[MAX_VERTEX_NUM];
+int low[MAX_VERTEX_NUM];
+
+void DFSArticul(ALGraph g,int v0);
+
+void findArticul(ALGraph g){
+    counts=1;
+    memset(visited,0,sizeof(visited));
+    visited[0]=1;
+    ArcNode *p= g.vertices[0].firstArc->nextarc;
+    //默认是连通图，即每一个节点的邻接表都不为空（p!=NULL）
+    int v=p->adjvex;
+    DFSArticul(g,v);
+    if (counts<g.vexNum){//意味着从根节点的一个儿子访问不了所有的节点（即有其它儿子，必须通过其它儿子访问剩余节点）
+        printf("关键节点%d: %c\n",0,g.vertices[0].data);
+        while(p->nextarc){
+            p=p->nextarc;
+            int v=p->adjvex;
+            if (visited[v]==0){
+                DFSArticul(g,v);
+            }
+        }
+    }
+}
+
+void DFSArticul(ALGraph g,int v0){
+    int min;
+    visited[v0]=min=++counts;
+    for (ArcNode *p=g.vertices[v0].firstArc->nextarc;p;p=p->nextarc){
+        int w=p->adjvex;
+        if (!visited[w]){
+            DFSArticul(g,w);
+            if (low[w]<min){
+                min=low[w];
+            }
+            if(low[w]>=visited[v0]){
+                printf("关键节点%d: %c\n",v0,g.vertices[v0].data);
+            }
+        }else if(visited[w]<min){
+            min=visited[w];
+        }
+    }
+    low[v0]=min;
+}
+
+int main(){
+    ALGraph g;
+    createGraph(g);
+    findArticul(g);
+    return 0;
+}
+
+/*
+2
+13
+17
+65 66 67 68 69 70 71 72 73 74 75 76 77
+65 66
+65 76
+65 67
+65 70
+66 67
+66 68
+66 71
+66 72
+66 77
+68 69
+71 72
+71 73
+71 75
+72 75
+74 76
+74 77
+76 77
+*/
