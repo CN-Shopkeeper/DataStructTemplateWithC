3f874f72287d743139572c6091bfe551fac86e0c
shopkeeper <734212732@qq.com>
2021-05-15 10:56:37 +0800

    曦：邻接表增加有向网内容；增加关键路径

--- xi/graph/AdjacencyList.cpp
+++ xi/graph/AdjacencyList.cpp
@@ -7,7 +7,7 @@
 typedef struct ArcNode{
     int adjvex;
     struct ArcNode *nextarc;
-    InfoType *info;
+    InfoType info;
     ArcNode(){}
 }ArcNode;
 
@@ -34,6 +34,7 @@ typedef struct{
 Status createGraph(ALGraph &g);
 Status createDG(ALGraph &g);
 Status createUDG(ALGraph &g);
+Status createDN(ALGraph &g);
 int locateVex(ALGraph g,VertexType v);
 
 /**
@@ -66,6 +67,15 @@ void printGraph(ALGraph g){
             }
         }
         break;
+    case DN:
+        for (int i=0;i<g.vexNum;i++){
+            ArcNode *p=g.vertices[i].firstArc;
+            while (p->nextarc) {
+                p=p->nextarc;
+                printf("从%d到%d有一条重为%d弧权\n",g.vertices[i].data,g.vertices[p->adjvex].data,p->info);
+            }
+        }
+        break;
     default:
         break;
     }
@@ -78,7 +88,7 @@ void printGraph(ALGraph g){
  * @return Status 操作结果 
  */
 Status createGraph(ALGraph &g){
-    printf("输入构造的图的类型（无向图：%d；有向图：%d）\n",UDG,DG);
+    printf("输入构造的图的类型（无向图：%d；有向图：%d；有向网：%d）\n",UDG,DG,DN);
     scanf("%d",&g.kind);
     switch (g.kind){
     case UDG:
@@ -86,8 +96,12 @@ Status createGraph(ALGraph &g){
         return createUDG(g);
         break;
     case DG:
-        printf("构建一个无向网（无弧信息）\n");
+        printf("构建一个有向图\n");
         return createDG(g);
+        break;
+     case DN:
+        printf("构建一个有向网\n");
+        return createDN(g);
         break;
     default:
         printf("类型错误");
@@ -110,7 +124,7 @@ Status createDG(ALGraph &g){
     for (int i=0;i<g.vexNum;i++){
         g.vertices[i].firstArc=(ArcNode *)malloc(sizeof(ArcNode));
         g.vertices[i].firstArc->nextarc=NULL;
-        g.vertices[i].firstArc->info=NULL;
+        g.vertices[i].firstArc->info=0;
         scanf("%d",&g.vertices[i].data);
         g.mapping[g.vertices[i].data]=i;
     }
@@ -131,7 +145,7 @@ Status createDG(ALGraph &g){
 }
 
 /**
- * @brief 创建一个有向图
+ * @brief 创建一个无向图
  * 
  * @param g 图
  * @return Status 操作结果 
@@ -145,7 +159,7 @@ Status createUDG(ALGraph &g){
     for (int i=0;i<g.vexNum;i++){
         g.vertices[i].firstArc=(ArcNode *)malloc(sizeof(ArcNode));
         g.vertices[i].firstArc->nextarc=NULL;
-        g.vertices[i].firstArc->info=NULL;
+        g.vertices[i].firstArc->info=0;
         scanf("%d",&g.vertices[i].data);
         g.mapping[g.vertices[i].data]=i;
     }
@@ -166,6 +180,41 @@ Status createUDG(ALGraph &g){
         nextd->adjvex=is;
         g.vertices[id].firstArc->nextarc=nextd;
         //不输入弧信息
+    }
+    return OK;
+}
+
+/**
+ * @brief 创建一个有向网
+ * 
+ * @param g 图
+ * @return Status 操作结果 
+ */
+Status createDN(ALGraph &g){
+    printf("输入节点数量:\n");
+    scanf("%d",&g.vexNum);
+    printf("输入弧数量:\n");
+    scanf("%d",&g.arcNum);
+    printf("输入各个节点名称:\n");
+    for (int i=0;i<g.vexNum;i++){
+        g.vertices[i].firstArc=(ArcNode *)malloc(sizeof(ArcNode));
+        g.vertices[i].firstArc->nextarc=NULL;
+        g.vertices[i].firstArc->info=0;
+        scanf("%d",&g.vertices[i].data);
+        g.mapping[g.vertices[i].data]=i;
+    }
+    printf("输入各个弧（源节点 目标节点 权重）:\n");
+    for (int i=0;i<g.arcNum;i++){
+        int s,d,w;
+        scanf("%d%d%d",&s,&d,&w);
+        int is=locateVex(g,s);
+        int id=locateVex(g,d);
+        ArcNode *p=g.vertices[is].firstArc->nextarc;
+        ArcNode *next=(ArcNode *)malloc(sizeof(ArcNode));
+        next->nextarc=p;
+        next->adjvex=id;
+        next->info=w;
+        g.vertices[is].firstArc->nextarc=next;
     }
     return OK;
 }
